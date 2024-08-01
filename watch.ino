c08aaa6cbd6d30036c43e84ee4ce723de57aaa29
nerdwithcomputers <hpscigeek@gmail.com>
2024-08-01 13:24:50 -0400

    proper formatting of dates and strings and things

--- watch.ino
+++ watch.ino
@@ -4,7 +4,7 @@
 
 TTGOClass *ttgo;
 
-uint8_t hour, minute, sec, day, month;
+String hour, minute, sec, day, month;
 
 void setup() {
   ttgo = TTGOClass::getWatch();
@@ -12,8 +12,7 @@ void setup() {
   ttgo->rtc->check();
   ttgo->rtc->syncToSystem();
   ttgo->tft->setTextFont(7);
-  ttgo->tft->setTextColor(0x19bdbd, TFT_BLACK);
-  ttgo->tft->setTextSize(1);
+  ttgo->tft->setTextColor(0x229c, TFT_BLACK);
   ttgo->openBL();
   
 }
@@ -25,25 +24,19 @@ void loop() {
 }
 
 void dispTime() {
-  byte xpos = 80;
-  byte ypos = 40;
+  ttgo->tft->setTextSize();
+  byte xpos = 5;
+  byte ypos = 5;
 
   RTC_Date tnow = ttgo->rtc->getDateTime();
-  hour = tnow.hour;
-  minute = tnow.minute;
-  sec = tnow.second;
-  day = tnow.day;
-  month = tnow.month;
-  String time = String(hour) + ":" + String(minute);
-  ttgo->tft->drawString(time, xpos, ypos);
+  month = (tnow.month < 10) ? String(0)+String(tnow.month) : String(tnow.month);;
+  day = (tnow.day < 10) ? String(0)+String(tnow.day) : String(tnow.day);;
+  hour = String(tnow.hour - 4);
+  minute = (tnow.minute < 10) ? String(0)+String(tnow.minute) : String(tnow.minute);;
+  sec = (tnow.second < 10) ? String(0)+String(tnow.second) : String(tnow.second);
+  
+  
 
-  // if(hour<10) ttgo->tft->drawChar('0', xpos, ypos);
-  // ttgo->tft->drawNumber(hour, xpos, ypos);
-  // ttgo->tft->drawChar(':', xpos+60, ypos);
-  /*
-  if(minute<10) ttgo->tft->drawChar('0',xpos+25,ypos);
-  ttgo->tft->drawNumber(minute,xpos+25,ypos);
-  if(sec<10) ttgo->tft->drawChar('0',xpos+30,ypos);
-  ttgo->tft->drawNumber(sec,xpos+30,ypos);
-  */
+  String time = hour + ":" + minute + ":" + sec;
+  ttgo->tft->drawString(time, xpos, ypos);
 }
