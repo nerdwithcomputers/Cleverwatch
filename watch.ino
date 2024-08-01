f7865eef28dfef548bcd34435213b2cfa714d505
nerdwithcomputers <hpscigeek@gmail.com>
2024-08-01 12:08:24 -0400

    c++ sucks

--- watch.ino
+++ watch.ino
@@ -1,14 +1,13 @@
 #include "config.h"
 #include <soc/rtc.h>
 #include <time.h>
+#include <string>
+#include <ostream>
 
 TTGOClass *ttgo;
+String string;
 
 uint8_t hour, minute, sec, day, month;
-byte colon = 0;
-
-byte xpos = 80;
-byte ypos = 40;
 
 void setup() {
   ttgo = TTGOClass::getWatch();
@@ -29,14 +28,26 @@ void loop() {
 }
 
 void dispTime() {
+  byte xpos = 80;
+  byte ypos = 40;
+
   RTC_Date tnow = ttgo->rtc->getDateTime();
   hour = tnow.hour;
   minute = tnow.minute;
   sec = tnow.second;
   day = tnow.day;
   month = tnow.month;
+  std::ostringstream oss;
+  oss << hour << ':' << minute;
+  ttgo->tft->drawString(oss.str(), xpos, ypos);
 
-  if(hour < 10) ttgo->tft->drawChar('0', xpos, ypos);
-  ttgo->tft->drawNumber(hour, xpos+10, ypos);
-  
+  // if(hour<10) ttgo->tft->drawChar('0', xpos, ypos);
+  // ttgo->tft->drawNumber(hour, xpos, ypos);
+  // ttgo->tft->drawChar(':', xpos+60, ypos);
+  /*
+  if(minute<10) ttgo->tft->drawChar('0',xpos+25,ypos);
+  ttgo->tft->drawNumber(minute,xpos+25,ypos);
+  if(sec<10) ttgo->tft->drawChar('0',xpos+30,ypos);
+  ttgo->tft->drawNumber(sec,xpos+30,ypos);
+  */
 }
